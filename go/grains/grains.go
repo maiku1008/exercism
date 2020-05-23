034b05a1ee88efe654b7774c737245f6db1250cd
Michael Cuffaro <michael@cuffaro.com>
2020-05-23 21:02:24 +0200

    graaaaaains

---
+++ go/grains/grains.go
@@ -0,0 +1,18 @@
+package grains
+
+import "fmt"
+
+// Square returns the number of grains on the chosen square
+// Returns an error if the square is out of accepted range
+func Square(square int) (uint64, error) {
+	if !((square >= 1) && (square <= 64)) {
+		return 0, fmt.Errorf("square out of range 1-64")
+	}
+
+	return 1 << (square - 1), nil
+}
+
+// Total sums all the grains on the board
+func Total() uint64 {
+	return 1<<64 - 1
+}
