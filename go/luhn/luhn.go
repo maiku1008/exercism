81f69965a2c7e69eaa8c500af93f9eec6f07af32
Michael Cuffaro <michael@cuffaro.com>
2020-05-23 18:07:52 +0200

    finish luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -9,45 +9,36 @@ import (
 func Valid(input string) bool {
 
 	// Remove whitespace
-	input = strings.Replace(input, " ", "", -1)
+	input = strings.ReplaceAll(input, " ", "")
 
 	// Check length
 	if len(input) < 2 {
 		return false
 	}
 
-	// Convert the string into digits.
-	// return false if the character is not a number.
-	var digits = make([]int, len(input))
-	for i, v := range input {
-		d, err := strconv.Atoi(string(v))
+	var (
+		even = len(input)%2 == 0
+		sum  int
+	)
+
+	for _, v := range input {
+		// Convert string value to int.
+		// If the value is not a number, return false.
+		digit, err := strconv.Atoi(string(v))
 		if err != nil {
 			return false
 		}
-		digits[i] = d
-	}
 
-	// Double every second digit starting from the right
-	i := len(digits) - 2
-	for i >= 0 {
-		double := digits[i] * 2
-		if double > 9 {
-			double -= 9
+		// Determine if we need to double our digit according to even bool
+		if even {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
 		}
-		digits[i] = double
-		i -= 2
+		even = !even
+		sum += digit
 	}
 
-	// Sum all digits
-	var sum int
-	for _, v := range digits {
-		sum += v
-	}
-
-	// Validate
-	if sum%10 != 0 {
-		return false
-	}
-
-	return true
+	return sum%10 == 0
 }
