2550d7fc877fe8e6d5b83f861ce200e795e84bcc
Michael Cuffaro <michael@cuffaro.com>
2020-05-25 15:40:15 +0200

    finish leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,14 +1,6 @@
 package leap
 
+// IsLeapYear return whether or not a year is a leap year
 func IsLeapYear(year int) bool {
-	switch {
-	case (year%4 == 0) && (year%100 == 0) && (year%400 == 0):
-		return true
-	case (year%4 == 0) && (year%100 == 0):
-		return false
-	case (year%4 == 0):
-		return true
-	default:
-		return false
-	}
+	return (year%4 == 0 && year%100 != 0) || year%400 == 0
 }
