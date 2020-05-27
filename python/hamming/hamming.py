a758641fde5274a64657f34c2dd0d2c6e2a19536
Michael Cuffaro <michael@cuffaro.com>
2020-05-27 08:25:17 +0200

    final hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,16 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Value must have the same length")
 
-    return len([i for i in range(len(strand_a)) if strand_a[i] != strand_b[i]])
-
-
-# def distance(strand_a, strand_b):
-#     if len(strand_a) != len(strand_b):
-#         raise ValueError("Value must have the same length")
-#
-#     diff = 0
-#     for i in range(len(strand_a)):
-#         if strand_a[i] != strand_b[i]:
-#             diff += 1
-#
-#     return diff
+    return len([strand for strand in zip(strand_a, strand_b) if strand[0] != strand[1]])
