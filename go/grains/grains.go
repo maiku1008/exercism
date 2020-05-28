c484d309c13766e6fb108bac6317ed4b292ffbd9
Michael Cuffaro <michael@cuffaro.com>
2020-05-28 09:21:09 +0200

    fixups

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -5,8 +5,8 @@ import "fmt"
 // Square returns the number of grains on the chosen square
 // Returns an error if the square is out of accepted range
 func Square(square int) (uint64, error) {
-	if !((square >= 1) && (square <= 64)) {
-		return 0, fmt.Errorf("square out of range 1-64")
+	if square < 1 || square > 64 {
+		return 0, fmt.Errorf("square %d out of range 1-64", square)
 	}
 
 	return 1 << (square - 1), nil
--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,4 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Values must have the same length")
 
-    return sum([1 for a, b in zip(strand_a, strand_b) if a != b])
+    return sum(a != b for a, b in zip(strand_a, strand_b))
