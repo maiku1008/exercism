8d966c3e6b87995842382d2bcf5b61b5f3e016ac
Michael Cuffaro <michael@cuffaro.com>
2020-06-05 21:48:40 +0200

    improve tree building

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,6 +1,10 @@
 package tree
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
 
 // Record contains information about a possible node
 type Record struct {
@@ -16,49 +20,76 @@ type Node struct {
 
 // Build builds the tree
 func Build(records []Record) (*Node, error) {
+	// Return if records is empty
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	root, err := setRoot(records)
-	if err != nil {
-		return nil, err
-	}
+	// Sort the records
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
 
+	var root *Node
 	for _, record := range records {
-		// Is root, skip it
+		// Set Root node
 		if record.ID == 0 {
+			switch {
+			case root != nil:
+				return nil, errors.New("root node exists and cannot be duplicated")
+			case record.ID == 0 && record.Parent != 0:
+				return nil, errors.New("node with id 0 has parent")
+			case record.ID == 0 && record.Parent == 0:
+				root = &Node{
+					ID: record.ID,
+				}
+			}
 			continue
 		}
 
-		// Check if the parent ID is the same as root node
-		if record.Parent == root.ID {
-			adoptChild(root, &Node{ID: record.ID})
+		// Check the record's parent, and match it with the right node.
+		parent, err := findNodeByID(record.Parent, root)
+		if err != nil {
+			return nil, err
 		}
-
-		// Check if the parent ID is in the root node's children ?
+		// We need to append the ID of the record to the correct parent node (record.Parent)
+		err = adoptChild(parent, &Node{ID: record.ID})
+		if err != nil {
+			return nil, err
+		}
 	}
 	return root, nil
 }
 
-// A helper function that adopts a child and would check for errors at some point
-func adoptChild(parent, child *Node) {
-	// TODO: Check if child is already there
+// A helper function that adopts a child if it's not there already
+func adoptChild(parent, child *Node) error {
+	for _, member := range parent.Children {
+		if member.ID == child.ID {
+			return fmt.Errorf("duplicate child: %d", child.ID)
+		}
+	}
 	parent.Children = append(parent.Children, child)
+
+	return nil
 }
 
-// A helper function that sets the root before dealing with the children
-func setRoot(records []Record) (*Node, error) {
-	var root *Node
-	for _, record := range records {
-		switch {
-		case record.ID == 0 && record.Parent != 0:
-			return nil, errors.New("node with id 0 has parent")
-		case record.ID == 0 && record.Parent == 0:
-			root = &Node{
-				ID: record.ID,
-			}
+// findNodeByID looks for a node with the id
+func findNodeByID(id int, start *Node) (*Node, error) {
+	if start == nil {
+		return nil, errors.New("starting node is nil")
+	}
+
+	// Found
+	if id == start.ID {
+		return start, nil
+	}
+
+	for _, node := range start.Children {
+		// Found
+		if node.ID == id {
+			return node, nil
 		}
 	}
-	return root, nil
+
+	return nil, fmt.Errorf("could not find node with id: %d ", id)
 }
