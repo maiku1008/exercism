0bd87d69847b24b5782dbe89e047be8911b2f1a5
Michael Cuffaro <michael@cuffaro.com>
2020-06-19 09:30:41 +0200

    resistors javascript

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+const resistors = new Map([
+  ['black', 0],
+  ['brown', 1],
+  ['red', 2],
+  ['orange', 3],
+  ['yellow', 4],
+  ['green', 5],
+  ['blue', 6],
+  ['violet', 7],
+  ['grey', 8],
+  ['white', 9],
+]);
+
+export const colorCode = (color) => resistors.get(color);
+
+export const COLORS = [...resistors.keys()];
