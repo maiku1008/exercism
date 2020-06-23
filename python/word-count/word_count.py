bd9cee3d3246298a66c52c957f304992fe2282bf
Michael Cuffaro <michael@cuffaro.com>
2020-06-23 22:11:30 +0200

    correct word_count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,13 @@
 from collections import Counter
+import string
 
-# Custom punctuation string without single quote '
-punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
 
 def count_words(sentence):
-    # Translate anything found in constant punctuation into whitespace
-    table = str.maketrans({k: " " for k in punctuation})
+    # Translate anything found punctuation into whitespace
+    table = str.maketrans({k: " " for k in string.punctuation.replace("'", "")})
     sentence = sentence.translate(table)
 
     # Strip single quotes surrounding words
-    s = [w.strip("'") for w in sentence.lower().split()]
+    stripped = [w.strip("'") for w in sentence.lower().split()]
 
-    return dict(Counter(s))
+    return dict(Counter(stripped))
