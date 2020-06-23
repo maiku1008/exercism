3e7e9b9d502900637d64ae88e43519f5dc8d6650
Michael Cuffaro <michael@cuffaro.com>
2020-06-23 15:55:58 +0200

    resistor again

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,20 +1,12 @@
-const resistors = new Map([
-    ['black', '0'],
-    ['brown', '1'],
-    ['red', '2'],
-    ['orange', '3'],
-    ['yellow', '4'],
-    ['green', '5'],
-    ['blue', '6'],
-    ['violet', '7'],
-    ['grey', '8'],
-    ['white', '9'],
-]);
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export const decodedValue = (colors) => {
-    let result = "";
-    for(var i in colors.slice(0, 2)){
-        result += resistors.get(colors[i]);
+    let a = []
+    for (var i in colors.slice(0, 2)) {
+        a.push(COLORS.indexOf(colors[i]))
     }
-    return parseInt(result);
+    if (a.length > 1) {
+        a[0] *= 10
+    }
+    return a.reduce((total, amount) => total + amount)
 }
