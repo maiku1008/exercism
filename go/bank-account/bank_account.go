0eeb98a51a1cb77a002b07ce90942ad67e5cfe90
Michael Cuffaro <michael@cuffaro.com>
2020-06-30 10:56:43 +0200

    final bank account

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -4,9 +4,9 @@ import "sync"
 
 // Account represents a bank account
 type Account struct {
-	active  bool
+	sync.RWMutex
+	closed  bool
 	balance int64
-	mu      sync.Mutex
 }
 
 // Open creates a new bank account
@@ -15,16 +15,15 @@ func Open(initialDeposit int64) *Account {
 		return nil
 	}
 	return &Account{
-		active:  true,
 		balance: initialDeposit,
 	}
 }
 
 // Balance returns the balance of an account
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	if !a.active {
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
 		return 0, false
 	}
 	return a.balance, true
@@ -32,9 +31,9 @@ func (a *Account) Balance() (balance int64, ok bool) {
 
 // Deposit adds an amount of an account's balance
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	if !a.active {
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
 		return 0, false
 	}
 	if a.balance+amount < 0 {
@@ -46,11 +45,11 @@ func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
 
 // Close closes a bank account
 func (a *Account) Close() (payout int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	if !a.active {
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
 		return 0, false
 	}
-	a.active = false
+	a.closed = true
 	return a.balance, true
 }
