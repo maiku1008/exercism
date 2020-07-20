903ed659bd2b33c204a6d7ba5ad7982321744857
Michael Cuffaro <michael@cuffaro.com>
2020-07-20 08:52:57 +0200

    kindergarden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -23,20 +23,18 @@ PLANT_TYPES = {
 
 class Garden:
     def __init__(self, diagram: str, students=None):
-
         students = sorted(students if students is not None else CHILDREN)
-        self.plant_table = {student: [None] * 4 for student in students}
+        self.plant_table = {student: [] for student in students}
 
-        diagrams = [diag for diag in diagram.splitlines()]
-        size = len(diagrams[0])
-        zipped = list(
-            zip(*[[dg[i : i + 2] for i in range(0, size, 2)] for dg in diagrams]),
-        )
-        grouped = ["".join(group) for group in zipped]
-
-        for i, group in enumerate(grouped):
-            for j, plant in enumerate(group):
-                self.plant_table[students[i]][j] = PLANT_TYPES[plant]
+        for line in diagram.splitlines():
+            student_index, increment = 0, False
+            for char in line:
+                self.plant_table[students[student_index]].append(PLANT_TYPES[char])
+                if increment:
+                    student_index += 1
+                    increment = False
+                else:
+                    increment = True
 
     def plants(self, name) -> list:
         return self.plant_table[name]
