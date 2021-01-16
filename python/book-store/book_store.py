6f8a2b6141a45100ab7621d95bce8858b7f84c68
Michael Cuffaro <michael@cuffaro.com>
2021-01-16 08:32:03 +0100

    feat: book store

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,23 +1,24 @@
 from collections import Counter
 
+SINGLE_ITEM_PRICE = 800
 DISCOUNTED = {
-    1: 1 * 800 * 1.00,  # 800
-    2: 2 * 800 * 0.95,  # 1520
-    3: 3 * 800 * 0.90,  # 2160
-    4: 4 * 800 * 0.80,  # 2560
-    5: 5 * 800 * 0.75,  # 3000
+    1: 1 * SINGLE_ITEM_PRICE * 1.00,  # 800
+    2: 2 * SINGLE_ITEM_PRICE * 0.95,  # 1520
+    3: 3 * SINGLE_ITEM_PRICE * 0.90,  # 2160
+    4: 4 * SINGLE_ITEM_PRICE * 0.80,  # 2560
+    5: 5 * SINGLE_ITEM_PRICE * 0.75,  # 3000
 }
 
 
-def total(basket):
-    return get_total(sorted(basket))
+def total(basket: list) -> int:
+    return get_total(basket)
 
 
-def get_total(books):
+def get_total(books: list) -> int:
     basket = Counter(books)
-    total = len(books) * 800
+    total = len(books) * SINGLE_ITEM_PRICE
     for count in range(len(basket), 1, -1):
         group = basket - Counter(item for item, _ in basket.most_common(count))
-        group_books = sorted(group.elements())
+        group_books = list(group.elements())
         total = min(total, DISCOUNTED[count] + get_total(group_books))
     return total
